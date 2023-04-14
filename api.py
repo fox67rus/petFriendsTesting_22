a0dd552686dc7bbec1598daeb13dc2e73699dcc6
Elena <fox67rus@mail.ru>
2023-04-14 14:36:47 +0300

    описание методов

--- api.py
+++ api.py
@@ -13,13 +13,14 @@ class PetFriends:
 
     def get_api_key(self, email: str, password: str) -> json:
         """
-        Получение ключа API.
+        Получение ключа API. Передаёт в заголовках емейл и пароль,
+        возвращает код статуса запроса и ключ аутентификации в формате json,
+        либо текстом
         """
         headers = {
             'email': email,
             'password': password,
         }
-
         res = requests.get(self.base_url + 'api/key', headers=headers)
         status = res.status_code
 
@@ -31,18 +32,19 @@ class PetFriends:
         return status, result
 
     def get_list_of_pets(self, auth_key: json, filter: str = '') -> json:
-        """Метод делает запрос к API сервера и возвращает статус запроса и результат в формате JSON
-        со списком найденных питомцев, совпадающих с фильтром. На данный момент фильтр может иметь
-        либо пустое значение - получить список всех питомцев, либо 'my_pets' - получить список
-        собственных питомцев
+        """Метод делает запрос к API сервера с ключом аутентификации в заголовках
+        и значением параметра filter в строке запроса.
+        Возвращает статус запроса и список найденных питомцев, совпадающих с фильтром, в формате JSON или в виде текста.
+        На данный момент фильтр может иметь либо пустое значение - получить список всех питомцев,
+        либо 'my_pets' - получить список собственных питомцев
         """
 
         headers = {'auth_key': auth_key['key']}
         filter = {'filter': filter}
 
         res = requests.get(self.base_url + 'api/pets', headers=headers, params=filter)
-        status = res.status_code
 
+        status = res.status_code
         try:
             result = res.json()
         except json.decoder.JSONDecodeError:
