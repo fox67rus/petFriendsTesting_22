4f5a09a714b4a1a600c743bd22f770570f12f84d
Elena <fox67rus@mail.ru>
2023-04-14 14:15:16 +0300

    настройка импортов, фикстура получения ключа

--- conftest.py
+++ conftest.py
@@ -1,20 +1,5 @@
 import pytest
-from datetime import datetime
 from tests.test_pet_friends import pf, valid_password, valid_email
-
-
-# После прохождения теста на жёстком диске появляется файл log.txt, в котором две секции:
-# в первой перечислены заголовки запроса, параметры пути, параметры строки и тело запроса; во второй — код ответа, тело ответа.
-
-# Среди тестов есть хотя бы по одному, помеченному декораторами:
-# @pytest.mark.xfail, @pytest.mark.skip, @pytest.mark.[<имя_пользовательской_группы>].
-
-# В файлах проекта присутствует pytest.ini, в котором перечислены пользовательские группы из предыдущего критерия.
-
-# После запуска тестов появляется файл log.txt, который логирует запросы и ответы к сервису приложения.
-
-# В логах секция запросов обязательно содержит: заголовки, параметры пути, параметры строки и тело запроса.
-# В логах секция ответа содержит код ответа и тело ответа.
 
 
 @pytest.fixture()
@@ -24,23 +9,3 @@ def get_key():
     assert status == 200
     assert 'key' in key
     return key
-
-@pytest.fixture(autouse=True)
-def time_delta():
-    start_time = datetime.now()
-    yield
-    end_time = datetime.now()
-    print (f"\nТест шел: {end_time - start_time}")
-
-@pytest.fixture
-def log(request):
-    start_time = datetime.now()
-    yield
-    end_time = datetime.now()
-
-    print()  # название теста
-
-    with open('log.txt', 'a', encoding="utf-8") as f:
-        f.write(f'Тест {request.function.__name__}. Начало в {start_time}, окончание в {end_time} , тест шёл {end_time - start_time} \n')
-
-
