e012ceb037b58b2741b0023b7a5a0d6c5a8f5f55
Elena <fox67rus@mail.ru>
2023-04-14 16:06:27 +0300

    логи

--- conftest.py
+++ conftest.py
@@ -1,3 +1,6 @@
+import functools
+from datetime import datetime
+
 import pytest
 from tests.test_pet_friends import pf, valid_password, valid_email
 
@@ -9,3 +12,41 @@ def get_key():
     assert status == 200
     assert 'key' in key
     return key
+
+
+@pytest.fixture
+def log(request):
+    start_time = datetime.now()
+    yield
+    end_time = datetime.now()
+
+    print()  # название теста
+
+    with open('log.txt', 'a', encoding="utf-8") as f:
+        f.write(
+            f'Тест {request.function.__name__}. Начало в {start_time}, окончание в {end_time} , тест шёл {end_time - start_time} \n')
+
+
+def log_api(func):
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        signature = func(*args, **kwargs)
+        status = str(signature[0])
+        result = str(signature[1])
+        request = str(signature[2:4])
+        responce_headers = str(signature[4:])
+        with open('log.txt', 'w', encoding='utf8') as f:
+            f.write(f'''Информация запроса:
+------------------
+Статус запроса:
+{status}
+Параметры запроса:
+{request}
+Информация ответа:
+------------------
+Тело ответа:
+{result}
+Заголовок ответа:
+{responce_headers}''')
+
+    return wrapper
