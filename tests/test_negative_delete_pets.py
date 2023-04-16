7e8a2198b60b1fccea15f3a5d6ade6cfa4e4721f
Elena <fox67rus@mail.ru>
2023-04-16 14:09:57 +0300

    Негативные тесты удаления питомцев - чужие питомцы

--- tests/test_negative_delete_pets.py
+++ tests/test_negative_delete_pets.py
@@ -10,16 +10,15 @@ from api import pf
 def test_unsuccessful_delete_another_user_pet(get_key):
     """Проверяем возможность удаления питомца другого пользователя. Ожидается отказ доступа"""
 
-    # Получаем ключ auth_key и запрашиваем список всех питомцев
-
-    _, all_pets = pf.get_list_of_pets(get_key, '')
+    # Получаем список всех питомцев на сайте
+    _, all_pets, _ = pf.get_list_of_pets(get_key)
 
     # Берём id первого питомца из общего списка и отправляем запрос на удаление
     pet_id = all_pets['pets'][0]['id']
-    status, _ = pf.delete_pet(get_key, pet_id)
-
+    status, _, _ = pf.delete_pet(get_key, pet_id)
     # Ещё раз запрашиваем список всех питомцев
-    _, my_pets = pf.get_list_of_pets(get_key)
+    _, all_pets, _ = pf.get_list_of_pets(get_key)
 
-    # Проверяем что статус ответа не равен 200
-    assert status != 200
+    # Проверяем статус ответа и что выбранный питомец не удалён
+    assert status == 400
+    assert pet_id in all_pets
