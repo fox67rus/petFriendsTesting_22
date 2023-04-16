f1fe6567899c309203c5627dcc8c334c8883ddbf
Elena <fox67rus@mail.ru>
2023-04-16 13:59:26 +0300

    функция для логирования запросов и ответов

--- config.py
+++ config.py
@@ -1,3 +1,5 @@
+import functools
+
 valid_email = 'test@qiott.com'
 valid_password = 'Qwerty_123'
 
@@ -19,3 +21,29 @@ def chinese_chars():
 
 def special_chars():
     return '|\\/!@#$%^&*()-_=+`~?"№;:[]{}'
+
+
+def log_test(func):
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        response = func(*args, **kwargs)
+        status = str(response[0])
+        result = str(response[1])
+        response_headers = str(response[2])
+        with open('log.txt', 'w', encoding='utf8') as f:
+            f.write(f'''Запрос:
+                        ------------------
+                        Заголовки запроса:
+                        Параметры пути: 
+                        Параметры строки:
+                        Тело запроса:                      
+
+                        Ответ:
+                        ------------------
+                        Код ответа: {status}
+                        Тело ответа:
+                        {result}
+                        Заголовки ответа:
+                        {response_headers}
+                    ''')
+    return wrapper
