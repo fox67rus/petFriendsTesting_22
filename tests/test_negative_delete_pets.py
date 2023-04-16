59c6df5454ab64abf6a1bcfac3d7b7bd0964c632
Elena <fox67rus@mail.ru>
2023-04-16 14:32:47 +0300

    Негативные тесты удаления питомцев - некорректный ключ аутентификации

--- tests/test_negative_delete_pets.py
+++ tests/test_negative_delete_pets.py
@@ -2,10 +2,10 @@
 import pytest
 
 from api import pf
+from config import generate_string, russian_chars, chinese_chars, special_chars
 
 
 @pytest.mark.skip(reason="Баг в продукте - https://petfriends.skillfactory.ru/my_pets")
-@pytest.mark.negative
 @pytest.mark.delete
 def test_unsuccessful_delete_another_user_pet(get_key):
     """Проверяем возможность удаления питомца другого пользователя. Ожидается отказ доступа"""
@@ -22,3 +22,41 @@ def test_unsuccessful_delete_another_user_pet(get_key):
     # Проверяем статус ответа и что выбранный питомец не удалён
     assert status == 400
     assert pet_id in all_pets
+
+
+@pytest.mark.parametrize("invalid_auth_key",
+                         [
+                             '',
+                             '123',
+                             generate_string(255),
+                             generate_string(1001),
+                             russian_chars(),
+                             russian_chars().upper(),
+                             chinese_chars(),
+                             special_chars()
+                         ], ids=[
+                            'key=empty',
+                            'key=digit',
+                            'key=string 255',
+                            'key=string 1001',
+                            'key=russian',
+                            'key=RUSSIAN',
+                            'key=chinese',
+                            'key=specials'])
+def test_delete_pet_invalid_key(get_key, invalid_auth_key):
+    """Проверяем возможность удаления питомца с некорректным ключом авторизации. Ожидается ответ 403"""
+
+    # Получаем список питомцев пользователя
+    _, my_pets, _ = pf.get_list_of_pets(get_key, 'my_pets')
+
+    # Проверяем - если список своих питомцев пустой, то добавляем нового и опять запрашиваем список своих питомцев
+    if len(my_pets['pets']) == 0:
+        pf.add_new_pet_without_photo(get_key, 'Тестик', 'кот', '2')
+        _, my_pets, _ = pf.get_list_of_pets(get_key, 'my_pets')
+
+    # Берём id первого питомца из списка и отправляем запрос на удаление с некорректным ключом
+    pet_id = my_pets['pets'][0]['id']
+    status, _, _ = pf.delete_pet(invalid_auth_key, pet_id)
+
+    # Проверяем статус ответа
+    assert status == 403
