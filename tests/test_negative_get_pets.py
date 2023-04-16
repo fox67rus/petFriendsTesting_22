e83e846407fa39fe6850cef04ce49430e4bdfc95
Elena <fox67rus@mail.ru>
2023-04-16 10:22:34 +0300

    Тестирование получения списка питомца с негативными параметрами

--- tests/test_negative_get_pets.py
+++ tests/test_negative_get_pets.py
@@ -2,6 +2,7 @@
 import pytest
 
 from api import pf
+from config import generate_string, russian_chars, chinese_chars, special_chars
 
 
 @pytest.mark.negative
@@ -12,6 +13,36 @@ def test_get_all_pets_with_invalid_key(filter=''):
     auth_key = {
         'key': 'very-invalid-api-key'
     }
-    status, result = pf.get_list_of_pets(auth_key, filter)
+    status, result, _ = pf.get_list_of_pets(auth_key, filter)
 
     assert status == 403
+
+
+@pytest.mark.xfail(reason='Баг в продукте - не верный ответ сервера')
+@pytest.mark.get
+@pytest.mark.parametrize("filter",
+                         [
+                             generate_string(255),
+                             generate_string(1001),
+                             russian_chars(),
+                             russian_chars().upper(),
+                             chinese_chars(),
+                             special_chars(),
+                             '123',
+                         ],
+                         ids=
+                         [
+                             '255 symbols',
+                             'more than 1000 symbols',
+                             'russian',
+                             'RUSSIAN',
+                             'chinese',
+                             'specials',
+                             'digit',
+                         ])
+def test_get_list_of_pets(get_key, filter):
+    """
+    Проверка негативных значений параметра filter. Статус код ответа 400, а не 500.
+    """
+    status, result, _ = pf.get_list_of_pets(get_key, filter)
+    assert status == 400
