7414d61010b0f519508e325c752ac8e464132cbb
Elena <fox67rus@mail.ru>
2023-04-16 20:03:02 +0300

    доработка функции получения ключа для удобного логирования

--- tests/test_positive_get_key.py
+++ tests/test_positive_get_key.py
@@ -2,19 +2,19 @@
 import pytest
 
 from api import pf
-from config import valid_email, valid_password
+from config import valid_email, valid_password, log_test
 
 
+@log_test
 @pytest.mark.get
 def test_get_api_key_for_valid_user(email=valid_email, password=valid_password):
-    """
-    Проверяем что запрос api ключа возвращает статус 200 и в результате содержится слово key
-    """
+    """Проверяем что запрос api ключа возвращает статус 200 и в результате содержится слово key"""
 
     # Отправляем запрос и сохраняем полученный ответ с кодом статуса в status, а текст ответа в result
-    status, result, response_headers = pf.get_api_key(email, password)
+    status, result, response_headers, headers = pf.get_api_key(email, password)
 
     # Сверяем статус и результат
     assert status == 200
     assert 'key' in result
     assert response_headers['Content-Type'] == 'application/json' or 'application/xml'
+    return status, result, headers, response_headers
